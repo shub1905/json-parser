96dbe4b4f4005b7aba06e70debeab38fb0097866
Shubham Bansal <shub1905@gmail.com>
2015-02-22 17:30:10 +0530

    mark 2

--- jsonParser.cpp
+++ jsonParser.cpp
@@ -3,6 +3,7 @@
 #include <map>
 #include <string>
 #include <stack>
+#include <sstream>
 using namespace std;
 
 class jsonTree
@@ -22,20 +23,149 @@ class jsonTree
         {
             parseArray(stream, vectorList);
         }
+        else if (type == "int")
+        {
+            intVal = stringToInt(stream);
+        }
+        else if (type == "bool")
+        {
+            boolVal = stringToBool(stream);
+        }
+        else if (type == "string")
+        {
+            stringVal = stream;
+        }
+        else // if (type == "object")
+        {
+            parseJsonObject(stream);
+        }
+
     }
 	private:
     void parseArray(string &stream, vector<jsonTree> & listObjects)
     {
         vector<string> objectsArray;
         getTokensArray(stream,objectsArray);
+        
+        for (int i=0; i<objectsArray.size(); i++)
+        {
+            string typeChild;
+            getType(objectsArray[i], typeChild);
+            
+            jsonTree * temp = new jsonTree();
+            temp -> loads(objectsArray[i]);
+            /*
+            if (typeChild == "int")
+            {
+                temp -> intVal = stringToInt(objectsArray[i]);
+            }
+            else if (typeChild == "bool")
+            {
+                temp -> boolVal = stringToBool(objectsArray[i]);
+            }
+            else if (typeChild == "string")
+            {
+                temp -> stringVal = objectsArray[i];
+            }
+            else // if(typeChild == "object" || typeChild == "array")
+            {
+
+            }*/
+            listObjects.push_back(*temp);
+        }
+
+    }
+    
+    bool stringToBool(string value)
+    {
+        for (int i=0;i<value.size();i++)
+        {
+            if (value[i] == 't')
+                return true;
+            else if (value[i] == 'f')
+                return false;
+            else if (value[i] == ' ')
+                continue;
+            else
+                break;
+        }
+        //write error handling code
+        return false;
+    }
+    
+    int stringToInt(string value)
+    {
+        stringstream ss;
+        ss<<value;
+        int val;
+        ss>>val;
+        return val;
     }
 
     void getTokensArray(string &stream, vector<string> &objectsArray)
     {
+        /* input = [A,B,C....]
+         output = vector<string> (A, B , C..) */
         
+        stack<char> bracketMatcher;
+        int start=1, end = -1;
+        for (int i=1; i<stream.size(); i++)
+        {
+            if (stream[i] == '[' || stream[i] == '{')
+            {
+                bracketMatcher.push(stream[i]);
+                start = i;
+            }
+            else if( (stream[i] == ',' || stream[i] == ']') && bracketMatcher.size() == 0)
+            {
+                end = i;
+                objectsArray.push_back(stream.substr(start,end));
+            }
+            else if (stream[i] == ']' || stream[i] == '}')
+            {
+                if (bracketMatcher.top() != stream[i])
+                {
+                    cout<<"Error"<<endl;
+                    return;
+                    //add more error handling code here
+                }
+            }
+        }
     }
     
-    void parseJson(string stream)
+    void getType(string &stream, string &type)
+    {
+        for (int i=0; i<stream.size(); i++)
+        {
+            if (stream[i] == '{')
+            {
+                type = "object";
+                break;
+            }
+            else if (stream[i] == '[')
+            {
+                type = "array";
+                break;
+            }
+            else if (stream[i] == ',' || stream[i] == '}')
+            {
+                type = "int";
+                break;
+            }
+            else if (stream[i] == '"')
+            {
+                type = "string";
+                break;
+            }
+            else if (stream[i] =='t' || stream[i] == 'f')
+            {
+                type = "bool";
+                break;
+            }
+        }
+    }
+
+    void parseJsonObject(string stream)
     {
         string key;
         string type;
@@ -102,37 +232,6 @@ class jsonTree
         return true;
     }
 
-    void getType(string &stream, string &type)
-    {
-        for (int i=0; i<stream.size(); i++)
-        {
-            if (stream[i] == '{')
-            {
-                type = "object";
-                break;
-            }
-            else if (stream[i] == '[')
-            {
-                type = "array";
-                break;
-            }
-            else if (stream[i] == ',' || stream[i] == '}')
-            {
-                type = "int";
-                break;
-            }
-            else if (stream[i] == '"')
-            {
-                type = "string";
-                break;
-            }
-            else if (stream[i] =='t' || stream[i] == 'f')
-            {
-                type = "bool";
-                break;
-            }
-        }
-    }
     bool getToken(string &stream, string &key, string &type)
     {
         getType(stream, type);
